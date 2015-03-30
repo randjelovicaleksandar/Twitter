61b8b6f302a181c9bf8082f651cff7e32004578d
randjelovicaleksandar <randjelovic8913@fon.bg.ac.rs>
2015-03-31 01:46:40 +0200

    Dodata nova klasa Twitter u paket com.twitter sa celokupnim kodom.

---
+++ Twitter/src/com/twitter/Twitter.java
@@ -0,0 +1,47 @@
+package com.twitter;
+
+import java.util.LinkedList;
+import com.twitter.poruke.TwitterPoruka;
+
+public class Twitter {
+
+	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
+
+	public LinkedList<TwitterPoruka> vratiSvePoruke() {
+		return poruke;
+	}
+
+	public void unesi(String korisnik, String poruka) {
+		// Pravi se nova poruka i puni podacima.
+		TwitterPoruka tp = new TwitterPoruka();
+		tp.setKorisnik("korisnik");
+		tp.setPoruka(poruka);
+		// Poruka se unosi u listu na kraj
+		poruke.addLast(tp);
+	}
+
+	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
+		if (tag == null || tag.isEmpty())
+			throw new RuntimeException("Morate uneti tag");
+		// Ako je maxBroj <=0, vraca maxBroj se postavlja na 100 poruka
+		if (maxBroj <= 0)
+			maxBroj = 100;
+		// Pomocna promenljiva koja predstavlja brojac upisanih poruka
+		int brojac = 0;
+		// Pomocni niz koja predstavlja rezultat pretrage tj. sadrzace
+		// sve poruke koje u sebi imaju zadati tag
+		TwitterPoruka[] rezultat = new TwitterPoruka[maxBroj];
+		// Pretrazuju se poruke i traze se one koje sadrze tag.
+		// Ako se nadje neka takva, i ako nije prekoracen maxBroj
+		// ona se upisuje u niz. Ako je prekoracen maxBroj,pretraga
+		// se prekida.
+		for (int i = 0; i < poruke.size(); i++)
+			if (poruke.get(i).getPoruka().indexOf(tag) != -1)
+				if (brojac < maxBroj) {
+					rezultat[brojac + 1] = poruke.get(i);
+					brojac++;
+				} else
+					break;
+		return rezultat;
+	}
+}
